e5b539bae190073d4ba1bccbd66e39521f8a1c1b
md al-amin <muhammadalamin809@gmail.com>
2023-06-09 00:36:00 +0800

    footer added

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -17,7 +17,7 @@ const PopularClasses = () => {
             </div>
             <div className="p-4 md:p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-12">
                 {
-                    classes.map(c => <div key={c._id}>
+                    classes.map(c => <div className="h-100" key={c._id}>
 
                         <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                 <img className="rounded-t-lg w-full h-60" src={c.image} alt="" />
@@ -26,10 +26,7 @@ const PopularClasses = () => {
                                     <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Instructor Name: {c.instructor_name}</h5>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Available Seat: {c.available_seats}</p>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: ${c.price}</p>
-                                {/* <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                </a> */}
+                              
                             </div>
                         </div>
 
---
+++ src/Pages/Share/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import { FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 text-black py-8 mx-4 md:mx-0  mt-10">
+            <div className="container mx-auto px-4">
+                <div className="flex flex-wrap justify-between">
+                    <div className="w-full md:w-1/4 mb-4">
+                        <a href="#">
+                            <img src="http://shippo.imoodev.com/wp-content/uploads/2023/05/shippo-logo.png" alt="Website Logo" className="h-10 inline-block" />
+                            <h3 className='text-2xl font-bold mt-4 '> <span className='text-blue-600'>Welcome to Our best Football Academy</span> </h3>
+                        </a>
+                       
+                    </div>
+                    <div className="w-full md:w-1/4 mb-4">
+                        <h4 className="text-black text-xl font-bold mb-4">Contact</h4>
+                        <p>123 Main Street</p>
+                        <p>City, State 12345</p>
+                        <p>Phone: (123) 456-7890</p>
+                        <p>Email: info@example.com</p>
+                    </div>
+                    <div className="w-full md:w-1/4 mb-7">
+                        <h4 className="text-black text-xl font-bold mb-4">Follow Us</h4>
+                        <div className="flex space-x-4">
+                            <a href="#" className="text-blue-600">
+                                <FaFacebook className='w-7 h-7'></FaFacebook>
+                            </a>
+                            <a href="#" className="text-blue-600">
+                                <FaInstagram className='w-7 h-7'></FaInstagram>
+                            </a>
+                            <a href="#" className="text-blue-600">
+                                <FaTwitter className='w-7 h-7'></FaTwitter>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/4 mb-4">
+                        <h4 className="text-black text-xl font-bold mb-4">Subscribe</h4>
+                        <p>Stay up to date with our latest Football Academy and offers.</p>
+                        <form className="mt-2">
+                            <input type="email" placeholder="Enter your email" className="w-full bg-gray-700 text-gray-300 px-4 py-2 rounded-lg"/>
+                                <button type="submit" className="mt-2 btn btn-primary">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className='text-center'>
+            <p className="mt-4">© 2023 Football Academy. All rights reserved.</p>
+            </div>
+        </footer>
+  );
+};
+
+export default Footer;
